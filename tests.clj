0a415ee9724d3cfa72be0eaf84b9759ec8150055
Teodor Heggelund <git@teod.eu>
2022-04-03 17:54:20 +0200

    Test getting versions for library

--- tests.clj
+++ tests.clj
@@ -1,9 +1,10 @@
 (ns tests
   (:require
    [babashka.fs :as fs]
-   [babashka.process :refer [tokenize]]
+   [babashka.process :refer [check process tokenize]]
    [babashka.tasks :as tasks]
    [clojure.edn :as edn]
+   [clojure.string :as str]
    [clojure.test :as t :refer [deftest is]]))
 
 (load-file "neil")
@@ -21,6 +22,17 @@
 (deftest add-dep-test
   (let [{:keys [edn]} (neil "add dep clj-kondo/clj-kondo")]
     (is (-> edn :deps (get 'clj-kondo/clj-kondo)))))
+
+(deftest dep-versions-test
+  (let [dep-versions (fn [lib & args]
+                       (-> (process (concat ["./neil" "dep" "versions" lib] args) {:out :string})
+                           check :out str/split-lines))
+        hiccup-versions (dep-versions "hiccup/hiccup")]
+    (is (seq (dep-versions "org.clojure/clojure"))
+        "We're able to find at least one Clojure version")
+    (is (= 3
+           (count (dep-versions "hiccup/hiccup" ":limit" 3)))
+        "We're able to find exactly 3 hiccup versions")))
 
 (when (= *file* (System/getProperty "babashka.file"))
   (t/run-tests *ns*))
