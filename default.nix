9d41052ffcb3f1daac03c810afbaffcf08c118eb
Yisrael Dov Lebow <lebow@lebowtech.com>
2022-04-04 22:53:53 +0300

    add nix-shell environment setup

---
+++ default.nix
@@ -0,0 +1,8 @@
+{ pkgs ?  import (fetchTarball "https://channels.nixos.org/nixos-21.11/nixexprs.tar.xz"){}}:
+pkgs.mkShell{
+  nativeBuildInputs = with pkgs; [
+    clojure
+    babashka
+    clj-kondo
+  ];
+}
