576d5992790de1bb9cb345d4a613bcb9d763061d
Ag Ibragimov <agzam.ibragimov@gmail.com>
2022-05-13 14:05:06 -0500

    adds comments to explain tests better

--- neil-tests.el
+++ neil-tests.el
@@ -35,51 +35,65 @@
     (spy-on #'shell-command-to-string
             :and-call-fake
             (lambda (command)
+              ;; shell-command-to-string may get called multiple times, first to search
+              ;; for packages, second time to get versions of a selected package,
+              ;; arguments and the results differ every time
               (setf shell-cmd-calls (1+ shell-cmd-calls))
-              (if (eq shell-cmd-calls 1)
-                  (progn
-                    (expect command :to-equal "/bin/neil dep search test-pkg")
-                    (concat
-                     ":lib foo/test-pkg :version 1.0.0 :description \"good lib\"\n"
-                     ":lib bar/awesome-test-pkg :version 2.1.0 :description \"better lib\"\n"))
-                (progn
-                  (expect command :to-equal "/bin/neil dep versions foo/test-pkg")
-                  (concat
-                   ":lib foo/test-pkg :version 1.0.0\n"
-                   ":lib bar/awesome-test-pkg :version 2.1.0\n")))))
+              (cond
+               ((eq shell-cmd-calls 1)
+                (expect command :to-equal "/bin/neil dep search test-pkg")
+                (concat
+                 ":lib foo/test-pkg :version 1.0.0 :description \"good lib\"\n"
+                 ":lib bar/awesome-test-pkg :version 2.1.0 :description \"better lib\"\n"))
+
+               ((eq shell-cmd-calls 2)
+                 (expect command :to-equal "/bin/neil dep versions foo/test-pkg")
+                 (concat
+                  ":lib foo/test-pkg :version 1.0.0\n"
+                  ":lib bar/awesome-test-pkg :version 2.1.0\n")))))
     (spy-on #'neil-search-annotation-fn)
     (spy-on #'completing-read
             :and-call-fake
             (lambda (prompt coll)
+              ;; `neil-find-clojure-package' pops up completion prompt multiple times,
+              ;; first to select from candidates matching the search term, second time, it
+              ;; prompts for a version of the package - but only if
+              ;; `neil-prompt-for-version-p' is not nil
               (setf prompt-calls (1+ prompt-calls))
-              (if (eq prompt-calls 1)
-                  (progn
-                    (expect prompt :to-equal "Found 2 matches for 'test-pkg':")
-                    (expect coll :to-equal
-                            '(("foo/test-pkg" (version . "1.0.0") (description . "\"good lib\""))
-                              ("bar/awesome-test-pkg" (version . "2.1.0") (description . "\"better lib\""))))
-                    "foo/test-pkg")
-                (progn
-                  ;; TODO: figure out how to assert the sorted order of completion candidates
-                  (expect prompt :to-equal "Choose version of foo/test-pkg:")
-                  "1.0.0")))))
+              (cond
+               ((eq prompt-calls 1)
+                (expect prompt :to-equal "Found 2 matches for 'test-pkg':")
+                (expect coll :to-equal
+                        '(("foo/test-pkg" (version . "1.0.0") (description . "\"good lib\""))
+                          ("bar/awesome-test-pkg" (version . "2.1.0") (description . "\"better lib\""))))
+                "foo/test-pkg")
+
+               ((eq prompt-calls 2)
+                ;; TODO: figure out how to assert the sorted order of completion candidates
+                (expect prompt :to-equal "Choose version of foo/test-pkg:")
+                "1.0.0")))))
+
   (it "shouldn't throw 'executable not found' error"
     (expect (neil-find-clojure-package "test-pkg") :not :to-throw))
+
   (it "for clojure-cli, without version prompt"
     (spy-on #'neil--identify-project-build-tool :and-return-value '(clojure-cli))
     (let ((neil-prompt-for-version-p nil))
       (expect (neil-find-clojure-package "test-pkg") :to-equal
               "foo/test-pkg {:mvn/version \"1.0.0\"}")))
+
   (it "for clojure-cli, with version prompt"
     (spy-on #'neil--identify-project-build-tool :and-return-value '(clojure-cli))
     (let ((neil-prompt-for-version-p t))
       (expect (neil-find-clojure-package "test-pkg") :to-equal
               "foo/test-pkg {:mvn/version \"1.0.0\"}")))
+
   (it "for lein, without version prompt"
     (spy-on #'neil--identify-project-build-tool :and-return-value '(lein))
     (let ((neil-prompt-for-version-p nil))
       (expect (neil-find-clojure-package "test-pkg") :to-equal
               "[foo/test-pkg \"1.0.0\"]")))
+
   (it "for lein, with version prompt"
     (spy-on #'neil--identify-project-build-tool :and-return-value '(lein))
     (let ((neil-prompt-for-version-p t))
