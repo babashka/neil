886cb5b20520f90445e9779db0aead3e3960e719
Radford Smith <11401+rads@users.noreply.github.com>
2022-08-07 08:23:10 -0700

    Add babashka.neil.version-test (#88)

---
+++ test/babashka/neil/test_runner.clj
@@ -0,0 +1,22 @@
+(ns babashka.neil.test-runner
+  (:require [babashka.fs :as fs]
+            [clojure.edn :as edn]
+            [clojure.test :as t]))
+
+(load-file (str (fs/parent *file*) "/../../../tests.clj"))
+
+(def test-namespaces
+  '[tests
+    babashka.neil.version-test])
+
+(doseq [ns test-namespaces]
+  (require ns))
+
+(defn run-tests [& {:keys [nses]}]
+  (let [selected-tests (if nses
+                         (edn/read-string nses)
+                         test-namespaces)
+        test-results (apply t/run-tests selected-tests)
+        {:keys [:fail :error]} test-results]
+    (when (pos? (+ fail error))
+      (throw (ex-info "Tests failed" {:babashka/exit 1})))))
