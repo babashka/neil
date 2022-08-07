566d1cd44ca5f01c2c6a3986e6d259ec33eab3fb
Radford Smith <11401+rads@users.noreply.github.com>
2022-08-07 08:41:15 -0700

    Add `babashka.neil.test-util`

--- test/babashka/neil/version_test.clj
+++ test/babashka/neil/version_test.clj
@@ -1,22 +1,6 @@
 (ns babashka.neil.version-test
-  (:require [babashka.fs :as fs]
-            [babashka.process :as process]
-            [babashka.tasks :as tasks]
-            [babashka.neil :as neil-main]
-            [clojure.edn :as edn]
+  (:require [babashka.neil.test-util :refer [neil test-file]]
             [clojure.test :refer [deftest is]]))
-
-(defn- test-file [name]
-  (doto (fs/file (fs/temp-dir) "neil" name)
-    (-> fs/parent (fs/create-dirs))
-    (fs/delete-on-exit)))
-
-(defn- neil [cli-args & {:keys [out] :or {out :edn}}]
-  (let [deps-file (str (test-file "deps.edn"))
-        cli-args' (concat (process/tokenize cli-args) [:deps-file deps-file])]
-    (binding [*command-line-args* cli-args']
-      (let [s (with-out-str (tasks/exec `neil-main/-main))]
-        {:out (if (#{:edn} out) (edn/read-string s) s)}))))
 
 (deftest version-option-test
   (let [{:keys [out]} (neil "--version" :out :string)]
