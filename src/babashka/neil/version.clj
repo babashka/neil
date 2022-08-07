cadf4f6e9bed03dc5a30d60544b12d202898b8b8
Radford Smith <11401+rads@users.noreply.github.com>
2022-08-07 10:07:37 -0700

    Add auto-commit and tag

--- src/babashka/neil/version.clj
+++ src/babashka/neil/version.clj
@@ -1,6 +1,8 @@
 (ns babashka.neil.version
-  (:require [babashka.neil.common :as common]
+  (:require [babashka.fs :as fs]
+            [babashka.neil.common :as common]
             [borkdude.rewrite-edn :as r]
+            [babashka.process :refer [sh]]
             [clojure.edn :as edn]
             [clojure.pprint :as pprint]
             [clojure.string :as str]))
@@ -45,16 +47,61 @@ Bump the :version key in the project config.")))
 (def valid-version-keys
   #{:major :minor :patch})
 
+(defn git-opts [opts]
+  (let [root (str (fs/parent (:deps-file opts)))]
+    (when (seq root)
+      {:dir root})))
+
+(defn git-repo? [opts]
+  (str/blank? (:err (sh "git status --porcelain" (git-opts opts)))))
+
+(defn git-clean-working-directory? [opts]
+  (let [{:keys [out err]} (sh "git status --porcelain" (git-opts opts))]
+    (and (str/blank? err) (str/blank? out))))
+
+(defn version-map->str [{:keys [major minor patch qualifier]}]
+  (str (str/join "." [major minor patch])
+       (when qualifier (str "-" qualifier))))
+
+(defn git-add [opts]
+  (sh "git add deps.edn" (git-opts opts)))
+
+(defn git-commit [version opts]
+  (sh ["git" "commit" "-m" (version-map->str version)]
+      (git-opts opts)))
+
+(defn git-tag [version opts]
+  (sh ["git" "tag" (str "v" (version-map->str version))]
+      (git-opts opts)))
+
+(defn assert-clean-working-directory [opts]
+  (when (and (not (git-clean-working-directory? opts))
+             (not (:force opts)))
+    (throw (ex-info "Requires clean working directory unless --force is provided" {}))))
+
+(defn git-tag-version-enabled? [opts]
+  (and (git-repo? opts)
+       (not (false? (:git-tag-version opts)))
+       (not (:no-git-tag-version opts))))
+
 (defn run-sub-command [sub-command opts args]
-  (common/ensure-deps-file opts)
-  (let [deps-map (edn/read-string (slurp (:deps-file opts)))
-        deps-nodes (-> opts common/edn-string common/edn-nodes)
-        override (when (second args) (Integer/parseInt (second args)))
-        deps-nodes' (save-version-bump deps-nodes deps-map sub-command override)
-        before {:project {:version (current-version deps-map)}}
-        after {:project {:version (current-version (edn/read-string (str deps-nodes')))}}]
-    (spit (:deps-file opts) (str deps-nodes'))
-    (pprint/pprint {:before before :after after})))
+  (let [git-tag-version-enabled (git-tag-version-enabled? opts)]
+    (common/ensure-deps-file opts)
+    (when git-tag-version-enabled
+      (assert-clean-working-directory opts))
+    (let [deps-map (edn/read-string (slurp (:deps-file opts)))
+          deps-nodes (-> opts common/edn-string common/edn-nodes)
+          override (when (second args) (Integer/parseInt (second args)))
+          deps-nodes' (save-version-bump deps-nodes deps-map sub-command override)
+          before {:project {:version (current-version deps-map)}}
+          after-v (current-version (edn/read-string (str deps-nodes')))
+          after {:project {:version after-v}}]
+      (spit (:deps-file opts) (str deps-nodes'))
+      (when git-tag-version-enabled
+        (git-add opts)
+        (git-commit after-v opts)
+        (git-tag after-v opts))
+      (pprint/pprint {:before before :after after}))))
 
 (defn print-version [opts]
   (let [deps-map (edn/read-string (slurp (:deps-file opts)))]
@@ -70,3 +117,18 @@ Bump the :version key in the project config.")))
     (if-let [sub-command (valid-version-keys (some-> args first keyword))]
       (run-sub-command sub-command opts args)
       (run-root-command opts))))
+
+(comment
+  (def test-dir (str (fs/temp-dir) "/neil"))
+  (sh ["open" test-dir])
+  test-dir
+  (do
+    (require '[babashka.neil.test-util :as test-util]
+             '[babashka.fs :as fs])
+    (spit (str test-dir "/deps.edn") "{}")
+    (sh "git init" {:dir test-dir})
+    (sh "git commit -aum 'First commit'" {:dir test-dir})
+    (sh "git init" {:dir test-dir})
+    (println (file-seq (fs/file test-dir)))
+    (test-util/neil "version patch")
+    (sh "git log --oneline -n1" {:dir test-dir})))
--- test/babashka/neil/test_util.clj
+++ test/babashka/neil/test_util.clj
@@ -1,14 +1,21 @@
 (ns babashka.neil.test-util
   (:require [babashka.fs :as fs]
             [babashka.neil :as neil-main]
-            [babashka.process :as process]
+            [babashka.process :refer [sh] :as process]
             [babashka.tasks :as tasks]
             [clojure.edn :as edn]))
 
+(def test-dir (str (fs/file (fs/temp-dir) "neil")))
+
 (defn test-file [name]
-  (doto (fs/file (fs/temp-dir) "neil" name)
+  (doto (fs/file test-dir name)
     (-> fs/parent (fs/create-dirs))
     (fs/delete-on-exit)))
+
+(defn ensure-git-repo []
+  (sh "git init" {:dir test-dir})
+  (sh "git add ." {:dir test-dir})
+  (sh "git commit -m 'First commit'" {:dir test-dir}))
 
 (defn neil [cli-args & {:keys [out] :or {out :edn}}]
   (let [deps-file (str (test-file "deps.edn"))
