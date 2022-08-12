3b6ca23086b903d34f147ea0d82caf93641b7a7f
Radford Smith <11401+rads@users.noreply.github.com>
2022-08-11 18:30:58 -0700

    Add test assertion when `meta/version` is set

--- test/babashka/neil/version_test.clj
+++ test/babashka/neil/version_test.clj
@@ -1,10 +1,15 @@
 (ns babashka.neil.version-test
   (:require [babashka.tasks :as tasks]
             [babashka.neil :as neil]
+            [babashka.neil.meta :as meta]
             [clojure.string :as str]
             [clojure.test :refer [deftest is]]))
 
 (deftest version-root-test
   (binding [*command-line-args* ["version"]]
     (let [out (with-out-str (tasks/exec `neil/-main))]
-      (is (str/starts-with? out "neil :version-not-set\n")))))
+      (is (str/starts-with? out "neil :version-not-set\n"))))
+  (with-redefs [meta/version "1.0.0"]
+    (binding [*command-line-args* ["version"]]
+      (let [out (with-out-str (tasks/exec `neil/-main))]
+        (is (str/starts-with? out "neil 1.0.0\n"))))))
