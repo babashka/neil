8032039500d81368e817ee4fb09f7f86e2928706
Russell Matney <russell.matney@gmail.com>
2022-10-12 15:14:09 -0400

    test: coverage for :git/sha dep upgrades
    Writes a few tests to be sure upgrade :git/sha based deps work as
    expected (i.e. they should not move to :mvn deps).

--- test/babashka/neil/dep_upgrade_test.clj
+++ test/babashka/neil/dep_upgrade_test.clj
@@ -6,8 +6,7 @@
 
 (def test-file-path (str (test-util/test-file "deps.edn")))
 (defn get-dep-version [dep-name]
-  (-> test-file-path slurp edn/read-string
-      :deps (get dep-name) :mvn/version))
+  (-> test-file-path slurp edn/read-string :deps (get dep-name)))
 
 (deftest dep-upgrade-test
   (testing "a fresh project is up-to-date"
@@ -28,8 +27,9 @@
 
       ;; after a non-dry-run, the version should be changed
       (test-util/neil "dep upgrade" :deps-file test-file-path)
-      (is (not (= clj-kondo-version-original (get-dep-version 'clj-kondo/clj-kondo))))))
+      (is (not (= clj-kondo-version-original (get-dep-version 'clj-kondo/clj-kondo)))))))
 
+(deftest dep-upgrade-test-one-lib
   (testing "specifying :lib only updates one dep"
     (spit test-file-path "{}")
     (test-util/neil "dep add :lib clj-kondo/clj-kondo :version 2022.01.01" :deps-file test-file-path)
@@ -41,3 +41,29 @@
             fs-upgraded        (get-dep-version 'babashka/fs)]
         (is (not (= clj-kondo-original clj-kondo-upgraded)))
         (is (= fs-original fs-upgraded))))))
+
+(deftest dep-upgrade-test-maintain-vcs
+  (testing "upgrading a :git/sha dep should maintain :git/sha"
+    (spit test-file-path "{}")
+    (test-util/neil "dep add :lib clj-kondo/clj-kondo :latest-sha true" :deps-file test-file-path)
+    (let [clj-kondo-original (get-dep-version 'clj-kondo/clj-kondo)]
+      ;; this upgrade should return the same latest sha, NOT a :mvn/version
+      (test-util/neil "dep upgrade" :deps-file test-file-path)
+      (let [{:keys [git/url git/sha]} (get-dep-version 'clj-kondo/clj-kondo)]
+        (is url)
+        (is sha)
+        (is (= clj-kondo-original (get-dep-version 'clj-kondo/clj-kondo))))))
+
+  (testing "upgrading an older :git/sha dep should set the latest :git/sha"
+    (spit test-file-path "{}")
+    (test-util/neil "dep add :lib clj-kondo/clj-kondo :sha 6ffc3934cb83d2c4fff16d84198c73b40cd8a078"
+                    :deps-file test-file-path)
+    (let [clj-kondo-original (get-dep-version 'clj-kondo/clj-kondo)]
+      ;; here we upgrade and then assert that the sha is different, but still :git/sha based
+      (test-util/neil "dep upgrade" :deps-file test-file-path)
+      (let [{:keys [git/url git/sha]} (get-dep-version 'clj-kondo/clj-kondo)]
+        (is url)
+        (is sha)
+        ;; should be a different sha
+        (is (not (= sha (:git/sha clj-kondo-original)))))))
+  )
