f63c4d6bec8808ae5be376cfd5f55a7d163c99d8
Russell Matney <russell.matney@gmail.com>
2022-10-12 14:40:12 -0400

    test: add test for updating a single dep
    This refactors this test somewhat - the `let` vars were pulled into
    defs, which makes evaling commands in the tests themselves much easier.
    
    A new test was added to be sure specifying a single `:lib` only updates
    that dep, leaving others as they are.
    
    The existing tests were also both using dry-run - they've been
    refactored to run without --dry-run to be sure things are actually
    working, except where dry run is being tested explicitly.

--- test/babashka/neil/dep_upgrade_test.clj
+++ test/babashka/neil/dep_upgrade_test.clj
@@ -4,23 +4,40 @@
    [clojure.test :as t :refer [deftest is testing]]
    [clojure.edn :as edn]))
 
+(def test-file-path (str (test-util/test-file "deps.edn")))
+(defn get-dep-version [dep-name]
+  (-> test-file-path slurp edn/read-string
+      :deps (get dep-name) :mvn/version))
+
 (deftest dep-upgrade-test
-  (let [test-file-path (str (test-util/test-file "deps.edn"))
-        get-dep-version (fn [dep-name] (-> test-file-path slurp edn/read-string
-                                           :deps (get dep-name) :mvn/version))]
+  (testing "a fresh project is up-to-date"
+    (spit test-file-path "{}")
+    (test-util/neil "dep add :lib clj-kondo/clj-kondo" :deps-file test-file-path)
+    (let [clj-kondo-version-original (get-dep-version 'clj-kondo/clj-kondo)]
+      (test-util/neil "dep upgrade" :deps-file test-file-path)
+      (is (= clj-kondo-version-original (get-dep-version 'clj-kondo/clj-kondo)))))
 
-    (testing "a fresh project is up-to-date"
-      (spit test-file-path "{}")
-      (test-util/neil "dep add :lib clj-kondo/clj-kondo" :deps-file test-file-path)
-      (let [clj-kondo-version-original (get-dep-version 'clj-kondo/clj-kondo)
-            _ (test-util/neil "dep upgrade" :deps-file test-file-path :dry-run true)
-            clj-kondo-version-upgraded (get-dep-version 'clj-kondo/clj-kondo)]
-        (is (= clj-kondo-version-original clj-kondo-version-upgraded))))
+  (testing "an old dependency can be upgraded, and --dry-run is respected"
+    (spit test-file-path "{}")
+    (test-util/neil "dep add :lib clj-kondo/clj-kondo :version 2022.01.01" :deps-file test-file-path)
+    (let [clj-kondo-version-original (get-dep-version 'clj-kondo/clj-kondo)]
 
-    (testing "an old dependency can be upgraded"
-      (spit test-file-path "{}")
-      (test-util/neil "dep add :lib clj-kondo/clj-kondo :version 2022.01.01" :deps-file test-file-path)
-      (let [clj-kondo-version-original (get-dep-version 'clj-kondo/clj-kondo)
-            _ (test-util/neil "dep upgrade" :deps-file test-file-path :dry-run true)
-            clj-kondo-version-upgraded (get-dep-version 'clj-kondo/clj-kondo)]
-        (is (= clj-kondo-version-original clj-kondo-version-upgraded))))))
+      ;; should be the same version after --dry-run
+      (test-util/neil "dep upgrade" :deps-file test-file-path :dry-run true)
+      (is (= clj-kondo-version-original (get-dep-version 'clj-kondo/clj-kondo)))
+
+      ;; after a non-dry-run, the version should be changed
+      (test-util/neil "dep upgrade" :deps-file test-file-path)
+      (is (not (= clj-kondo-version-original (get-dep-version 'clj-kondo/clj-kondo))))))
+
+  (testing "specifying :lib only updates one dep"
+    (spit test-file-path "{}")
+    (test-util/neil "dep add :lib clj-kondo/clj-kondo :version 2022.01.01" :deps-file test-file-path)
+    (test-util/neil "dep add :lib babashka/fs :version 0.0.1" :deps-file test-file-path)
+    (let [clj-kondo-original (get-dep-version 'clj-kondo/clj-kondo)
+          fs-original        (get-dep-version 'babashka/fs)]
+      (test-util/neil "dep upgrade :lib clj-kondo/clj-kondo" :deps-file test-file-path)
+      (let [clj-kondo-upgraded (get-dep-version 'clj-kondo/clj-kondo)
+            fs-upgraded        (get-dep-version 'babashka/fs)]
+        (is (not (= clj-kondo-original clj-kondo-upgraded)))
+        (is (= fs-original fs-upgraded))))))
