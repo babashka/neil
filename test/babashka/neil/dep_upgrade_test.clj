a61584691eba58322ec193401ef29aeb001fa520
Michiel Borkent <michielborkent@gmail.com>
2023-10-26 11:22:35 +0200

    Fix test

--- test/babashka/neil/dep_upgrade_test.clj
+++ test/babashka/neil/dep_upgrade_test.clj
@@ -220,7 +220,7 @@
       (is (not (:git/tag kondo-upgrade)) ", there is no tag.")))
 
   (testing "when --unstable is set, upgrade to unstable hiccup versions"
-    (is (= {:mvn/version "2.0.0-RC1"}
+    (is (= {:mvn/version "2.0.0-RC2"}
            (neil/dep->upgrade {:lib 'hiccup/hiccup
                                :current {:mvn/version "1.0.0"}
                                :unstable true})))))
