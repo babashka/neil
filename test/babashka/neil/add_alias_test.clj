b669687df91276d4365899610894d4e25c9d105d
Teodor Heggelund <git@teod.eu>
2024-06-03 22:37:37 +0200

    Add examples of weird behavior as passing tests

--- test/babashka/neil/add_alias_test.clj
+++ test/babashka/neil/add_alias_test.clj
@@ -10,10 +10,53 @@
   (= (str/trim s1)
      (str/trim s2)))
 
-(deftest add-alias-str
+(deftest add-alias-str-good1
   (is (trim= "
 {:aliases
  {:kaocha ;; added by neil
   {:extra-deps {lambdaisland/kaocha {:mvn/version \"1.91.1392\"}}
    :main-opts [\"-m\" \"kaocha.runner\"]}}}"
              (:deps-file-str (neil/add-alias-str "{}" :kaocha kaocha-alias)))))
+
+(deftest add-alias-str-weird1
+  ;; weird space in between.
+  (is (trim= "
+{:aliases {:dev {}
+
+ :kaocha ;; added by neil
+ {:extra-deps {lambdaisland/kaocha {:mvn/version \"1.91.1392\"}}
+  :main-opts [\"-m\" \"kaocha.runner\"]}}}"
+             (:deps-file-str (neil/add-alias-str "{:aliases {:dev {}}}" :kaocha kaocha-alias)))))
+
+(deftest add-alias-str-weird2
+  ;; weird space in between.
+  (is (trim= "
+{:aliases
+ {:dev {}
+
+ :kaocha ;; added by neil
+ {:extra-deps {lambdaisland/kaocha {:mvn/version \"1.91.1392\"}}
+  :main-opts [\"-m\" \"kaocha.runner\"]}}}"
+             (:deps-file-str (neil/add-alias-str (str/trim "
+{:aliases
+ {:dev {}}}"
+                                                           )
+                                                 :kaocha kaocha-alias)))))
+
+(comment
+  (defn escape-quote [s]
+    (str/escape s {\" "\\\""}))
+
+  (-> "hello \"you\"!" escape-quote escape-quote escape-quote)
+  (def println2 (comp println escape-quote))
+
+  (println)
+
+  (-> (:deps-file-str (neil/add-alias-str (str/trim "
+{:aliases
+ {:dev {}}}"
+                                                    )
+                                          :kaocha kaocha-alias))
+      println2)
+
+  )
