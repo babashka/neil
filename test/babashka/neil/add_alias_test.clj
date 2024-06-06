509b5d9643cd3b3904f2e78c67e7bc90533b44d3
Teodor Heggelund <git@teod.eu>
2024-06-06 06:59:40 +0200

    Add green tests for new behavior

--- test/babashka/neil/add_alias_test.clj
+++ test/babashka/neil/add_alias_test.clj
@@ -11,85 +11,69 @@
   (= (str/trim s1)
      (str/trim s2)))
 
-(deftest add-alias-str-good1
-  (is (trim= "
-{:aliases
- {:kaocha ;; added by neil
-  {:extra-deps {lambdaisland/kaocha {:mvn/version \"1.91.1392\"}}
-   :main-opts [\"-m\" \"kaocha.runner\"]}}}"
-             (:deps-file-str (neil/add-alias-str "{}" :kaocha kaocha-alias)))))
+(deftest add-alias-str-empty-deps-edn
+    (is (trim= "
+{:aliases {:kaocha {:extra-deps {lambdaisland/kaocha {:mvn/version \"1.91.1392\"}}
+                    :main-opts [\"-m\" \"kaocha.runner\"]}}}
+"
+               (:deps-file-str (neil/add-alias-str "{}" :kaocha kaocha-alias)))))
 
-(deftest add-alias-str-weird1
-  ;; weird space in between.
-  (is (trim= "
-{:aliases {:dev {}
-
- :kaocha ;; added by neil
- {:extra-deps {lambdaisland/kaocha {:mvn/version \"1.91.1392\"}}
-  :main-opts [\"-m\" \"kaocha.runner\"]}}}"
-             (:deps-file-str (neil/add-alias-str "{:aliases {:dev {}}}" :kaocha kaocha-alias)))))
-
-(deftest add-alias-str-weird2
-  ;; weird space in between.
-  (is (trim= "
+(deftest add-alias-str-deps-edn-with-dev-alias
+    (is (trim= "
 {:aliases
  {:dev {}
+  :kaocha {:extra-deps {lambdaisland/kaocha {:mvn/version \"1.91.1392\"}}
+           :main-opts [\"-m\" \"kaocha.runner\"]}}}
+"
+               (:deps-file-str (neil/add-alias-str "{:aliases
+ {:dev {}}}" :kaocha kaocha-alias)))))
 
- :kaocha ;; added by neil
- {:extra-deps {lambdaisland/kaocha {:mvn/version \"1.91.1392\"}}
-  :main-opts [\"-m\" \"kaocha.runner\"]}}}"
-             (:deps-file-str (neil/add-alias-str (str/trim "
-{:aliases
- {:dev {}}}"
-                                                           )
-                                                 :kaocha kaocha-alias)))))
+(deftest add-alias-str-simplified-neil-deps-edn
+  (let [input-deps-edn (str/trim "
+{:deps {org.babashka/http-client {:mvn/version \"0.1.4\"}
+        org.babashka/cli {:mvn/version \"0.8.58\"}
+        cheshire/cheshire {:mvn/version \"5.11.0\"}
+        version-clj/version-clj {:mvn/version \"2.0.2\"}}
+ :aliases
+ {:dev
+  {:extra-paths [\"test\"]
+   :extra-deps {io.github.cognitect-labs/test-runner
+                {:git/url \"https://github.com/cognitect-labs/test-runner\"
+                 :git/tag \"v0.5.1\"
+                 :git/sha \"dfb30dd\"}}}}}
+")
+        expected-deps-edn (str/trim "
+{:deps {org.babashka/http-client {:mvn/version \"0.1.4\"}
+        org.babashka/cli {:mvn/version \"0.8.58\"}
+        cheshire/cheshire {:mvn/version \"5.11.0\"}
+        version-clj/version-clj {:mvn/version \"2.0.2\"}}
+ :aliases
+ {:dev
+  {:extra-paths [\"test\"]
+   :extra-deps {io.github.cognitect-labs/test-runner
+                {:git/url \"https://github.com/cognitect-labs/test-runner\"
+                 :git/tag \"v0.5.1\"
+                 :git/sha \"dfb30dd\"}}}
+  :kaocha {:extra-deps {lambdaisland/kaocha {:mvn/version \"1.91.1392\"}}
+           :main-opts [\"-m\" \"kaocha.runner\"]}}}
+")]
+    (is (trim= expected-deps-edn
+               (:deps-file-str (neil/add-alias-str input-deps-edn :kaocha kaocha-alias))))))
 
 (comment
+  ;; Some helper code to generate the strings for the tests
+
   (defn escape-quote [s]
     (str/escape s {\" "\\\""}))
 
-  (-> "hello \"you\"!" escape-quote escape-quote escape-quote)
   (def println2 (comp println escape-quote))
 
-  (println)
-
-  (-> (:deps-file-str (neil/add-alias-str (str/trim "
+  (let [s (str/trim "
 {:aliases
- {:dev {}}}"
-                                                    )
-                                          :kaocha kaocha-alias))
-      println2)
-
-  )
-
-(comment
-  ;; how do I use rewrite-edn?
-
-  (str (r/assoc (r/parse-string "{:x 1\n :y 2}") :key "value"))
-
-  (let [s (str/triml "
-{:deps {}
- :aliases {}}
-")]
-    (-> (r/parse-string s)
-        (r/assoc-in [:aliases :kaocha] {})
-        (r/assoc-in [:aliases :dev] {})
-        str
-        println))
-  ;; =>
-  ;; {:deps {}
-  ;;  :aliases {:kaocha {}
-  ;;            :dev {}}}
-
-  ;; this works just perfectly.
-
-  (let [s (str/triml "
-{:deps {}
- :aliases {:dev {}}}
-")]
-    (-> (r/parse-string s)
-        (r/assoc-in [:aliases :kaocha :extra-deps] {:lambdaisland/kaocha {:mvn/version "1.91.1392"}},)
-        (r/assoc-in [:aliases :kaocha :main-opts] ["-m" "kaocha.runner"])
-        str
+ {:dev {}}}
+")
+        ]
+    (-> (:deps-file-str (neil/add-alias-str s :kaocha kaocha-alias))
         println2))
-  )
+
+  :rcf)
