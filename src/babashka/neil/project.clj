6abac5d0274f97ec644af7df23cf4152d89f5beb
Bob <highpressurecarsalesman@gmail.com>
2024-10-12 23:59:10 -0400

    ignore missing deps file when running new

--- src/babashka/neil/project.clj
+++ src/babashka/neil/project.clj
@@ -4,38 +4,41 @@
    [borkdude.rewrite-edn :as r]
    [clojure.edn :as edn]))
 
-(defn resolve-deps-file [dir deps-file]
-  (if dir
-    (fs/file dir deps-file)
-    deps-file))
+(defn resolve-deps-file 
+  "Returns a file object for `deps-file` in the `dir` directory if that file exists, else nil."
+  [dir deps-file]
+  (let [resolved (if dir
+                   (fs/file dir deps-file)
+                   (fs/file deps-file))]
+    (when (fs/exists? resolved) resolved)))
 
 (defn ensure-neil-project [{:keys [dir deps-file]}]
-  (let [deps-file (resolve-deps-file dir deps-file)
-        deps-edn (slurp deps-file)
-        edn (edn/read-string deps-edn)]
-    (when-not (some-> edn :aliases :neil :project)
-      (let [existing-aliases (:aliases edn)
-            edn-nodes (r/parse-string deps-edn)
-            edn-nodes (cond-> edn-nodes
-                        (not (:aliases edn))
-                        (r/assoc :aliases (r/parse-string "\n {}"))
-                        (contains? existing-aliases :neil)
-                        (r/assoc-in [:aliases :neil] (r/parse-string "\n {}"))
-                        (not (some-> edn :aliases :neil :project))
-                        (r/assoc-in [:aliases :neil :project]
-                                    {}))]
-        (spit deps-file (str edn-nodes))))))
+  (when-let [deps-file (resolve-deps-file dir deps-file)]
+    (let [deps-edn (slurp deps-file)
+          edn (edn/read-string deps-edn)]
+      (when-not (some-> edn :aliases :neil :project)
+        (let [existing-aliases (:aliases edn)
+              edn-nodes (r/parse-string deps-edn)
+              edn-nodes (cond-> edn-nodes
+                          (not (:aliases edn))
+                          (r/assoc :aliases (r/parse-string "\n {}"))
+                          (contains? existing-aliases :neil)
+                          (r/assoc-in [:aliases :neil] (r/parse-string "\n {}"))
+                          (not (some-> edn :aliases :neil :project))
+                          (r/assoc-in [:aliases :neil :project]
+                                      {}))]
+          (spit deps-file (str edn-nodes)))))))
 
 (defn assoc-project-meta!
   "Updates deps-file's :neil :project `k` with `v`"
   [{:keys [dir deps-file k v]
     :as opts}]
   (ensure-neil-project opts)
-  (let [deps-file (resolve-deps-file dir deps-file)
-        deps-edn (slurp deps-file)
-        nodes (r/parse-string deps-edn)
-        nodes (r/assoc-in nodes [:aliases :neil :project k] v)]
-    (spit deps-file (str nodes))))
+  (when-let [deps-file (resolve-deps-file dir deps-file)]
+    (let [deps-edn (slurp deps-file)
+          nodes (r/parse-string deps-edn)
+          nodes (r/assoc-in nodes [:aliases :neil :project k] v)]
+      (spit deps-file (str nodes)))))
 
 (defn project-name [{:keys [deps-file]}]
   (-> (edn/read-string (slurp deps-file))
